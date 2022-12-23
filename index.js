81df7c2c7377f98fad4b85ab2fead162e5fa7c25
ckim812 <109961170+ckim812@users.noreply.github.com>
2022-12-23 08:11:36 -0600

    view all departments, roles employees, add department, role working; working on add employee

--- index.js
+++ index.js
@@ -1,34 +1,47 @@
 const inquirer = require("inquirer");
 const cTable = require("console.table");
 const db = require("./db/connection");
-let x = {};
+const { exit } = require("process");
 
-console.log("Welcome to the Employee Tracker Application!");
-init();
+console.log("\n\nWelcome to the Employee Tracker Application!\n");
+// init();
+addRole();
 
 function init() {
   inquirer
     .prompt([
       {
-        loop: true,
-        type: "list",
+        loop: false,
+        type: "rawlist",
         name: "task",
         message: "What would you like to do?",
         choices: [
-          "View All Employees",
-          "Add Employee",
-          "Update Employee Role",
-          "View all Roles",
-          "Add Role",
           "View All Departments",
-          "Add Department",
+          "View All Roles",
+          "View All Employees",
+          "Add a Department",
+          "Add a Role",
+          "Add an Employee",
+          "Update an Employee Role",
         ],
       },
     ])
     .then((data) => {
       switch (data.task) {
+        case "View All Departments":
+          viewDepartments();
+          break;
+        case "View All Roles":
+          viewRoles();
+          break;
         case "View All Employees":
           viewEmployees();
+          break;
+        case "Add Department":
+          addDepartment();
+          break;
+        case "Add Role":
+          addRole();
           break;
         case "Add Employees":
           viewEmployees();
@@ -36,26 +49,106 @@ function init() {
         case "Update Employee Role":
           viewEmployees();
           break;
-        case "View all Roles":
-          viewRoles();
-          break;
-        case "Add Role":
-          viewRoles();
-          break;
-        case "View All Departments":
-          viewDepartment();
-          break;
-        case "Add Department":
-          viewEmployees();
-          break;
       }
     });
 }
 
+//import database
+//use inquirer to ask questions
+//use async await with the databases
+
+//make another JS with questions?? (something to work towards)
+
+function viewDepartments() {
+  db.query(
+    "SELECT department.name AS Departments FROM department",
+    function (err, results) {
+      console.table(results);
+      init();
+    }
+  );
+}
+
+function viewRoles() {
+  db.query(
+    "SELECT role.title AS Role, role.salary AS Salary, department.name AS Department FROM role JOIN department ON role.department_id = department.id;",
+    function (err, results) {
+      console.table(results);
+      init();
+    }
+  );
+}
+
 function viewEmployees() {
-  db.query("SELECT employee.id AS ID, employee.first_name AS First, employee.last_name AS Last, role.title AS Role, department.name AS department, role.salary AS Salary, employee.manager_id AS Manager_ID FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id;", function (err, results) {
-    console.table(results);
-    init();
+  db.query(
+    "SELECT employee.id AS ID, employee.first_name AS First, employee.last_name AS Last, role.title AS Role, department.name AS Department, role.salary AS Salary, employee.manager_id AS Manager_ID FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id;",
+    function (err, results) {
+      console.table(results);
+      init();
+    }
+  );
+}
+
+function addDepartment() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is the name of the department? ",
+      },
+    ])
+    .then((data) => {
+      db.query(
+        `INSERT INTO department (name) VALUES (${data.name})`,
+        function (err, results) {
+          console.log("New Department Added");
+          init();
+        }
+      );
+    });
+}
+
+function addRole() {
+  db.query("SELECT name FROM department", function (err, results) {
+    let currentDepartments = [];
+    for (let i = 0; i < results.length; i++) {
+      currentDepartments.push(results[i].name);
+    }
+
+    console.log(currentDepartments);
+
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "title",
+          message: "What is the title of the role? ",
+        },
+        {
+          type: "input",
+          name: "salary",
+          message: "What is the salary of the role? ",
+        },
+        {
+          loop: true,
+          type: "list",
+          name: "department",
+          message: "Which department does the role belong to? ",
+          choices: currentDepartments,
+          //   choices: ["Sales", "Engineering", "Finance", "Legal"],
+        },
+      ])
+      .then((data) => {
+        departmentID = currentDepartments.indexOf(data.department) + 1;
+        db.query(
+          `INSERT INTO role (title, salary, department_id) VALUES ("${data.title}",${data.salary},${departmentID})`,
+          function (err, results) {
+            console.log("New Role Added");
+            init();
+          }
+        );
+      });
   });
 }
 
@@ -86,66 +179,3 @@ function viewEmployees() {
 //       const newRole = new Role(data.name);
 //     });
 // }
-
-function viewRoles() {
-  db.query(
-    "SELECT role.title AS Role, role.salary AS Salary, department.name AS Department FROM role JOIN department ON role.department_id = department.id;",
-    function (err, results) {
-      console.table(results);
-      init();
-    }
-  );
-}
-
-// function addRole() {
-//   inquirer
-//     .prompt([
-//       {
-//         type: "input",
-//         name: "title",
-//         message: "What is the title of the role? ",
-//       },
-//       {
-//         type: "input",
-//         name: "salary",
-//         message: "What is the salary of the role? ",
-//       },
-//       {
-//         loop: true,
-//         type: "list",
-//         name: "department",
-//         message: "Which department does the role belong to? ",
-//         choices: ["Sales", "Engineering", "Finance", "Legal"],
-//       },
-//     ])
-//     .then((data) => {
-//       const newRole = new Role(data.name);
-//     });
-// }
-
-function viewDepartment() {
-  db.query("SELECT department.name AS Departments FROM department", function (err, results) {
-    console.table(results);
-    init();
-  });
-}
-
-// function addDepartment() {
-//   inquirer
-//     .prompt([
-//       {
-//         type: "input",
-//         name: "name",
-//         message: "What is the name of the department? ",
-//       },
-//     ])
-//     .then((data) => {
-//       const newDepartment = new Department(data.name);
-//     });
-// }
-
-//import database
-//use inquirer to ask questions
-//use async await with the databases
-
-//make another JS with questions?? (something to work towards)
